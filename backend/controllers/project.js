072f1f4a8e4c4f072ea00e509c576bd80e6a6dbc
David Poza Suarez <pozasuarez@gmail.com>
2018-06-22 17:16:25 +0200

    método updateProject

--- backend/controllers/project.js
+++ backend/controllers/project.js
@@ -57,6 +57,21 @@ var controller = {
 
         });
     },
+    updateProject: function(req,res){
+        var projectId = req.params.id;
+        var update = req.body;
+
+        Project.findByIdAndUpdate(projectId, update, {new:true}, (err,projectUpdated) => {
+            if(err) return res.status(500). send({message: 'Error al devolver los datos.'});
+
+            if(!projectUpdated) return res.status(404).send({message: 'No existe el proyecto a actualizar'});
+
+            return res.status(200).send({
+                project: projectUpdated
+            });
+        });
+
+    }
 }
 
 module.exports = controller;
