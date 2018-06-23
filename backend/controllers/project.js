628c8c6912ca0ce7e8bb875354870750a0f7071e
David Poza Suarez <pozasuarez@gmail.com>
2018-06-23 13:34:16 +0200

    método upload-image y get-image

--- backend/controllers/project.js
+++ backend/controllers/project.js
@@ -1,5 +1,7 @@
 'use strict'
 var Project = require('../models/project')
+var fs = require('fs');
+var path = require('path');
 
 var controller = {
     home: function(req,res){
@@ -85,7 +87,58 @@ var controller = {
             });
         });
 
-    }
+    },
+	uploadImage: function(req, res){
+		var projectId = req.params.id;
+        var fileName = 'Imagen no subida...';
+        
+		if(req.files){
+			var filePath = req.files.image.path;
+			var fileSplit = filePath.split('\\');
+			var fileName = fileSplit[1];
+			var extSplit = fileName.split('\.');
+			var fileExt = extSplit[1];
+
+			if(fileExt == 'png' || fileExt == 'jpg' || fileExt == 'jpeg' || fileExt == 'gif'){
+
+				Project.findByIdAndUpdate(projectId, {image: fileName}, {new: true}, (err, projectUpdated) => {
+					if(err) return res.status(500).send({message: 'La imagen no se ha subido'});
+
+					if(!projectUpdated) return res.status(404).send({message: 'El proyecto no existe y no se ha asignado la imagen'});
+
+					return res.status(200).send({
+						project: projectUpdated
+					});
+				});
+
+			}else{
+				fs.unlink(filePath, (err) => {
+					return res.status(200).send({message: 'La extensión no es válida'});
+				});
+			}
+
+		}else{
+			return res.status(200).send({
+				message: fileName
+			});
+		}
+
+	},
+
+	getImageFile: function(req, res){
+		var file = req.params.image;
+		var path_file = './uploads/'+file;
+
+		fs.exists(path_file, (exists) => {
+			if(exists){
+				return res.sendFile(path.resolve(path_file));
+			}else{
+				return res.status(200).send({
+					message: "No existe la imagen..."
+				});
+			}
+		});
+	}
 }
 
 module.exports = controller;
